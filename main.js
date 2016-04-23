317454d07f0a8e0c2be412702cda23639d6c5d52
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-23 23:54:10 +0200

    add mainjs file with instructions

---
+++ main.js
@@ -0,0 +1,6 @@
+// function getting images form list and dynamically add to mozaik
+// in fuction setting number of images in row
+// counting width of image
+// counting numbers of rows
+// setting filters (grey, color etc.)
+// at beggining only same size pictures
