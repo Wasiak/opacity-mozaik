787ef5ee6659e6708d7598cae25929bcfb1d7951
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-24 23:03:36 +0200

    first function- not works

--- main.js
+++ main.js
@@ -4,3 +4,11 @@
 // counting numbers of rows
 // setting filters (grey, color etc.)
 // at beggining only same size pictures
+var picsInRow = 4
+var picWidth = 100 / picsInRow;
+var pictures = document.getElementsByTagName('img');
+console.log(pictures);
+pictures.forEach(function(img) {
+  console.log('elo', img)
+  img.style.width = picWidth + '%';
+});
