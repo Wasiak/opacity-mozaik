44ab7aabb7c542eb558e409d067f9ca875d99767
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-25 18:51:53 +0200

    counting images width, row number and images number

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 // function getting images form list and dynamically add to mozaik
 // in fuction setting number of images in row
-// counting width of image
-// counting numbers of rows
 // setting filters (grey, color etc.)
 // at beggining only same size pictures
+
 var picsInRow = 4
-var picWidth = 100 / picsInRow;
+var picWidth = Math.round(100 / picsInRow);
+var mainImage = document.getElementById('images-container');
+var rowNumber;
+var picturesNumber;
 var pictures = document.getElementsByTagName('img');
-console.log(pictures);
-pictures.forEach(function(img) {
-  console.log('elo', img)
+
+pictures[0].onload = function(evt) {
+  console.log(evt.target.offsetHeight);
+  rowNumber = Math.ceil(mainImage.offsetHeight / evt.target.offsetHeight);
+  picturesNumber = rowNumber * picsInRow;
+  // console.log(rowNumber, picturesNumber)
+}
+
+// make array from DOMelements
+var myDOMArray = Array.prototype.slice.call(pictures);
+
+myDOMArray.forEach(function(img) {
   img.style.width = picWidth + '%';
 });
