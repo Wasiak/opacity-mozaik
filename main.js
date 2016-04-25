3580718b1d945b36221c3ff5d429e6f6599fe616
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-25 19:15:05 +0200

    dynamically added pictures

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-// function getting images form list and dynamically add to mozaik
 // in fuction setting number of images in row
 // setting filters (grey, color etc.)
 // at beggining only same size pictures
+var imagesNames = ['1', '2', '3', '4'];
 
-var picsInRow = 4
+var picsInRow = 4;
 var picWidth = Math.round(100 / picsInRow);
 var mainImage = document.getElementById('images-container');
 var rowNumber;
 var picturesNumber;
-var pictures = document.getElementsByTagName('img');
 
-pictures[0].onload = function(evt) {
-  console.log(evt.target.offsetHeight);
-  rowNumber = Math.ceil(mainImage.offsetHeight / evt.target.offsetHeight);
-  picturesNumber = rowNumber * picsInRow;
-  // console.log(rowNumber, picturesNumber)
+var pic = document.createElement('img');
+mainImage.appendChild(pic);
+pic.src = 'images/' + imagesNames[0] + '.jpg';
+pic.style.width = picWidth + '%';
+
+var addPictures = function() {
+  var j = 1;
+  for (i = 1; i < picturesNumber; i++) {
+    var pic = document.createElement('img');
+    mainImage.appendChild(pic);
+    if (j > imagesNames.length - 1) {
+      j = 0;
+    }
+    pic.src = 'images/' + imagesNames[j] + '.jpg';
+    j++;
+  }
 }
 
-// make array from DOMelements
-var myDOMArray = Array.prototype.slice.call(pictures);
+pic.onload = function(evt) {
+  rowNumber = Math.ceil(mainImage.offsetHeight / evt.target.offsetHeight);
+  picturesNumber = rowNumber * picsInRow;
+  addPictures();
 
-myDOMArray.forEach(function(img) {
-  img.style.width = picWidth + '%';
-});
+  var pictures = document.getElementsByTagName('img');
+  // make array from DOMelements
+  var myDOMArray = Array.prototype.slice.call(pictures);
+
+  myDOMArray.forEach(function(img) {
+    img.style.width = picWidth + '%';
+  });
+}
+
+
