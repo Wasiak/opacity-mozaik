036ceecd873d0c615eb3d7d64af737c2dfe7636f
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-26 18:53:07 +0200

    canvas images first try

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 // at beggining only same size pictures
 var imagesNames = ['1', '2', '3', '4'];
 
-var picsInRow = 8;
-var picWidth = 100 / picsInRow;
+var picsInRow = 6;
+var picPercentageWidth = 100 / picsInRow;
+var picWidth;
+var picHeight;
 var mainImage = document.getElementById('images-container');
 var rowNumber;
 var picturesNumber;
@@ -12,24 +14,50 @@ var picturesNumber;
 var pic = document.createElement('img');
 mainImage.appendChild(pic);
 pic.src = 'images/' + imagesNames[0] + '.jpg';
-pic.style.width = picWidth + '%';
+pic.style.width = picPercentageWidth + '%';
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+var addImage = function(source, left, top){
+  // console.log(source)
+  image = new Image();
+  image.src = source;
+  image.onload = function(){
+    context.drawImage(image, left, top, picWidth, picHeight);
+  }
+}
 
 var addPictures = function() {
-  var j = 1;
-  for (i = 1; i < picturesNumber; i++) {
-    var pic = document.createElement('img');
-    mainImage.appendChild(pic);
-    if (j > imagesNames.length - 1) {
-      j = 0;
-    }
-    pic.src = 'images/' + imagesNames[j] + '.jpg';
+  var j = 0;
+  var left = 0;
+  var top = 0;
+  var t = 0;
+  var l = 0;
+  for (i = 0; i < picturesNumber; i++) {
+    j = j > imagesNames.length - 1 ? 0 : j;
+    
+    t = l > picsInRow -1 ? t += 1 : t;
+    l = l > picsInRow - 1 ? 0 : l;
+
+    left = l * picWidth;
+    top = t * picHeight;
+    // console.log(l, t);
+    // console.log(top, left)
+    
+    var src = 'images/' + imagesNames[j] + '.jpg';
+    console.log(src)
+    addImage(src, left, top);
     j++;
+    l++;
   }
 }
 
 pic.onload = function(evt) {
   rowNumber = Math.ceil(mainImage.offsetHeight / evt.target.offsetHeight);
   picturesNumber = rowNumber * picsInRow;
+  picHeight = evt.target.offsetHeight;
+  picWidth = (picPercentageWidth /100) * canvas.offsetWidth;
   addPictures();
 
   var pictures = document.getElementsByTagName('img');
@@ -37,7 +65,7 @@ pic.onload = function(evt) {
   var myDOMArray = Array.prototype.slice.call(pictures);
 
   myDOMArray.forEach(function(img) {
-    img.style.width = picWidth + '%';
+    img.style.width = picPercentageWidth + '%';
   });
 }
 
