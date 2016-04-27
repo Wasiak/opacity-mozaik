5f8d40cb19efcb15c2cc6d7b38f61b5c8208dade
Kuba Wasiak <qbawasiak@wp.pl>
2016-04-27 14:37:47 +0200

    code clean

--- main.js
+++ main.js
@@ -21,11 +21,9 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 var addImage = function(source, left, top){
-  console.log(source)
   image = new Image();
   image.src = source;
   image.onload = function(){
-    console.log(image.src);
     context.globalAlpha = 0.4;
     context.drawImage(this, left, top, picWidth, picHeight);
     context.globalAlpha = 1;
